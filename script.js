f7cb9c75b5beb0ad0b551cf26f624c4d3d8e6e10
w0rlz <unfriedhayden@gmail.com>
2025-09-21 14:31:19 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+let score = 0;
+let playerX = 50;
+let playerY = 50;
+const img = new Image();
+img.src = "data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' width='100' height='100'><rect width='100' height='100' fill='green'/></svg>";
+document.body.appendChild(img);
+function update() {
+  ctx.drawImage(img, playerX, playerY,32,32);
+  playerX += 1;
+  requestAnimationFrame(update);
+}
+update();
